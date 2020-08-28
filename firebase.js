e9d5ee1992a00e0abe787bdab4c8700343e3a160
mimi <mimsgh2018@gmail.com>
2020-08-28 14:49:04 +0000

    set up a boiler plate for tik tok clone

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD4IobUvaiXPRX-V71EgDk3a3-uHLwexqE",
+  authDomain: "tik-tok-clone-7a31f.firebaseapp.com",
+  databaseURL: "https://tik-tok-clone-7a31f.firebaseio.com",
+  projectId: "tik-tok-clone-7a31f",
+  storageBucket: "tik-tok-clone-7a31f.appspot.com",
+  messagingSenderId: "500186178170",
+  appId: "1:500186178170:web:9364d4a48761569b0c51c3",
+  measurementId: "G-4FJ3YJTCNG",
+};
---
+++ src/components/video/Video.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Video.css";
+
+/**
+ * @author
+ * @function Video
+ **/
+
+const Video = (props) => {
+  return (
+    <div className="video">
+      <h1>this is a video component</h1>
+    </div>
+  );
+};
+
+export default Video;
