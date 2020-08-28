9257f7bd0f996ec9f5e31b3b8c50e55a4ca0ab6f
mimi <mimsgh2018@gmail.com>
2020-08-28 16:14:04 +0000

    added a video component and styled it

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Video from "./components/video/Video";
 function App() {
   return (
     <div className="app">
-      <h1>tik tok clone</h1>
       <div className="app_videos">
         <Video />
         <Video />
