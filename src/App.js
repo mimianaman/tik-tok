56d4498ebedc44f185ee8d49a3e27c8f8a5f8814
mimi <mimsgh2018@gmail.com>
2020-08-29 03:15:53 +0000

    set up firebase data and added a few data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import db from "./firebase";
 import "./App.css";
 import Video from "./components/video/Video";
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  //useEffect fies once when the component loads but never again after//
+  useEffect(() => {
+    db.collection("videos").onSnapshot((snapshot) =>
+      setVideos(snapshot.docs.map((doc) => doc.data()))
+    );
+  }, [videos]);
   return (
     <div className="app">
       <div className="app_videos">
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        {videos.map(
+          ({ url, channel, description, likes, messages, shares }) => (
+            <Video
+              url={url}
+              channel={channel}
+              description={description}
+              likes={likes}
+              messages={messages}
+              shares={shares}
+            />
+          )
+        )}
       </div>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+import firebase from "firebase";
 const firebaseConfig = {
   apiKey: "AIzaSyD4IobUvaiXPRX-V71EgDk3a3-uHLwexqE",
   authDomain: "tik-tok-clone-7a31f.firebaseapp.com",
@@ -8,3 +9,9 @@ const firebaseConfig = {
   appId: "1:500186178170:web:9364d4a48761569b0c51c3",
   measurementId: "G-4FJ3YJTCNG",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
